a19161e3f77b121f1a664ba184dd844b561219b4
Aniurm <534147085@qq.com>
2023-11-28 13:00:32 +0800

    try to log current step

--- cenima/navi.swift
+++ cenima/navi.swift
@@ -10,8 +10,13 @@ import MapboxDirections
 import MapboxCoreNavigation
 import MapboxNavigation
 import CoreLocation
+import os
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, NavigationServiceDelegate {
+    let logger = Logger(subsystem: "ARTrack", category: "INFO")
+    
+    var navigationService: NavigationService?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,15 +38,23 @@ class ViewController: UIViewController {
             case .failure(let error):
                 print(error.localizedDescription)
             case .success(let response):
+                guard let route = response.routes?.first else { return }
+                // Create a navigation service
+                self?.navigationService = MapboxNavigationService(routeResponse: response, routeIndex: 0, routeOptions: routeOptions, simulating: .never)
+                self?.navigationService?.delegate = self
                 guard let self = self else { return }
-                // Pass the first generated route to the the NavigationViewController
-                let viewController = NavigationViewController(for: response, routeIndex: 0, routeOptions: routeOptions)
+                // Initialize the NavigationViewController with custom NavigationOptions
+                let navigationOptions = NavigationOptions(navigationService: self.navigationService)
+                let viewController = NavigationViewController(for: response, routeIndex: 0, routeOptions: routeOptions, navigationOptions: navigationOptions)
                 viewController.modalPresentationStyle = .fullScreen
                 self.present(viewController, animated: true, completion: nil)
             }
-            
-            func navigationService(_ service: NavigationService, didUpdate progress: RouteProgress, with location: CLLocation, rawLocation: CLLocation) {
-            }
         }
+    }
+
+    // NavigationServiceDelegate method
+    func navigationService(_ service: NavigationService, didUpdate progress: RouteProgress, with location: CLLocation, rawLocation: CLLocation) {
+        // log current step
+        logger.info("Current step: \(progress.currentLegProgress.currentStep.description)")
     }
 }
